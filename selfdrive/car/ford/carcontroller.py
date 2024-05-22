c1d271366e60829753ccc6d0b92b69d05792ddc0
alan-polk <56047663+alan-polk@users.noreply.github.com>
2024-05-22 17:12:45 -0500

    if steeringPressed and abs(steeringAngleDeg) > 45:

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -268,7 +268,7 @@ class CarController:
       steeringAngleDeg = CS.out.steeringAngleDeg
 
       # if a human turn is active, reset steering to prevent windup
-      if steeringPressed and abs(steeringAngleDeg) > 60:
+      if steeringPressed and abs(steeringAngleDeg) > 45:
         apply_curvature = 0
         self.new_actuators.humanTurnApplyCurvature = float(apply_curvature)
         ramp_type = 3
