111e5a4e63e4313531cb65e271212ae08fc0cb9b
zhuory <107243601+zhuory@users.noreply.github.com>
2026-02-22 20:12:33 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -24,9 +24,9 @@ from openpilot.selfdrive.modeld.cone_detections import decode_cone_detections
 from openpilot.selfdrive.modeld.lane_occupancy import compute_ego_lane_occupancy
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_VALS_FORD = [1.2, 1.0, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
+A_CRUISE_MAX_VALS = [3.0, 1.7, 1.3, 0.8, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+A_CRUISE_MAX_VALS_FORD = [3.0, 1.7, 1.3, 0.8, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+A_CRUISE_MAX_BP = [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
 CONTROL_N_T_IDX = ModelConstants.T_IDXS[:CONTROL_N]
 ALLOW_THROTTLE_THRESHOLD = 0.4
 MIN_ALLOW_THROTTLE_SPEED = 2.5
@@ -789,6 +789,7 @@ class LongitudinalPlanner:
 
     longitudinalPlan.aTarget = float(self.output_a_target)
     longitudinalPlan.shouldStop = bool(self.output_should_stop)
+
     longitudinalPlan.allowBrake = True
     longitudinalPlan.allowThrottle = bool(self.allow_throttle)
     longitudinalPlan.curveSpeedSource = int(getattr(self, "_curve_speed_source", 0))
