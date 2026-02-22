ee477e4ed4cc0d53452490a0130dcd49f3bc8316
zhuory <107243601+zhuory@users.noreply.github.com>
2026-02-22 20:15:36 +0800

    Update helpers.py

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -24,12 +24,12 @@ class DRIVER_MONITOR_SETTINGS:
   def __init__(self, device_type):
     self._DT_DMON = DT_DMON
     # ref (page15-16): https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
-    self._AWARENESS_TIME = 30. # passive wheeltouch total timeout
-    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 15.
-    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-    self._DISTRACTED_TIME = 11. # active monitoring total timeout
-    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+    self._AWARENESS_TIME = 600. # passive wheeltouch total timeout
+    self._AWARENESS_PRE_TIME_TILL_TERMINAL = 300.
+    self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 120.
+    self._DISTRACTED_TIME = 220. # active monitoring total timeout
+    self._DISTRACTED_PRE_TIME_TILL_TERMINAL = 160.
+    self._DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 120.
 
     self._FACE_THRESHOLD = 0.7
     self._EYE_THRESHOLD = 0.65
@@ -74,8 +74,8 @@ class DRIVER_MONITOR_SETTINGS:
     self._RECOVERY_FACTOR_MAX = 5.  # relative to minus step change
     self._RECOVERY_FACTOR_MIN = 1.25  # relative to minus step change
 
-    self._MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
-    self._MAX_TERMINAL_DURATION = int(30 / self._DT_DMON)  # not allowed to engage after 30s of terminal alerts
+    self._MAX_TERMINAL_ALERTS = 60  # not allowed to engage after 60 terminal alerts
+    self._MAX_TERMINAL_DURATION = int(600 / self._DT_DMON)  # not allowed to engage after 600s of terminal alerts
 
 class DistractedType:
   NOT_DISTRACTED = 0
