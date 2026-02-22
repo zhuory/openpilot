f9e0efe87b39e3ce229421ce98b834da38abddba
zhuory <107243601+zhuory@users.noreply.github.com>
2026-02-22 20:08:36 +0800

    Update api.py

--- common/api.py
+++ common/api.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta, UTC
 from openpilot.system.hardware.hw import Paths
 from openpilot.system.version import get_version
 
-API_HOST = os.getenv('API_HOST', 'https://api.commadotai.com')
+API_HOST = os.getenv('API_HOST', 'https://api.konik.ai')
 
        # name : jwt signature algorithm
 KEYS = {"id_rsa" : "RS256",
